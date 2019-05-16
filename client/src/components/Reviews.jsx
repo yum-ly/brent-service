b4889781e4f49e939534e9588710a0c4c9f37705
Brent Rusnell <blrusnell@gmail.com>
2019-05-16 17:24:36 -0500

    set up reviews component

---
+++ client/src/components/Reviews.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Reviews extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {}
+}
+
+export default Reviews;
+
---
+++ client/src/index.jsx
@@ -0,0 +1,1 @@
+ReactDOM.render('<Reviews />', document.getElementById('reviews'));
