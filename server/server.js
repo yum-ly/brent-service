40f37bdccb384fc328ae53c5fc0a3b849f3b472b
Brent Rusnell <blrusnell@gmail.com>
2019-05-29 11:19:26 -0500

    pls work

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
-app.use(express.static('dist'));
+app.use(express.static(__dirname + '/../client/dist'));
 //__dirname + '/../client/dist'
 
   
