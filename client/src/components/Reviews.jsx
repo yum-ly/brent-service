a286ad1544785dac775403359bfc776795811e53
Brent Rusnell <blrusnell@gmail.com>
2019-05-29 19:45:21 -0500

    changed the date to use month name, not number

--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -35,6 +35,47 @@ class Reviews extends React.Component {
         return num / 5;
     }
 
+    dateChanger(date) {
+        let month = date.slice(5, 7);
+
+        if (month === '01') {
+            return 'January ' + date.slice(8, 10) + ', ' + date.slice(0, 4);
+        }
+        if (month === '02') {
+            return 'February ' + date.slice(8, 10) + ', ' + date.slice(0, 4);
+        }
+        if (month === '03') {
+            return 'March ' + date.slice(8, 10) + ', ' + date.slice(0, 4);
+        }
+        if (month === '04') {
+            return 'April ' + date.slice(8, 10) + ', ' + date.slice(0, 4);
+        }
+        if (month === '12') {
+            return 'May ' + date.slice(8, 10) + ', ' + date.slice(0, 4);
+        }
+        if (month === '05') {
+            return 'June ' + date.slice(8, 10) + ', ' + date.slice(0, 4);
+        }
+        if (month === '07') {
+            return 'July ' + date.slice(8, 10) + ', ' + date.slice(0, 4);
+        }
+        if (month === '08') {
+            return 'August ' + date.slice(8, 10) + ', ' + date.slice(0, 4);
+        }
+        if (month === '09') {
+            return 'September ' + date.slice(8, 10) + ', ' + date.slice(0, 4);
+        }
+        if (month === '10') {
+            return 'October ' + date.slice(8, 10) + ', ' + date.slice(0, 4);
+        }
+        if (month === '11') {
+            return 'November ' + date.slice(8, 10) + ', ' + date.slice(0, 4);
+        }
+        if (month === '12') {
+            return 'December ' + date.slice(8, 10) + ', ' + date.slice(0, 4);
+        }
+    }
+
 
     render() {
         return(
@@ -45,7 +86,7 @@ class Reviews extends React.Component {
                         <div>
                             <img key={index} src={review.avatar} alt="Avatar" className="images"/>
                             <div key={index} className='names'>{review.user}</div>
-                            <div key={index} className='dates'>{review.date.slice(0, 10)}</div>
+                            <div key={index} className='dates'>{this.dateChanger(review.date)}</div>
                             <div className='startext'>
                                 <span key={index} className='rating'>{review.stars}</span>
                                 <span key={index} className='text'>{review.text}</span>
