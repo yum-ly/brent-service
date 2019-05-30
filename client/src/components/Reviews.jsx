2661cbf16c97e40a080ef169347aba738a8709a5
Brent Rusnell <blrusnell@gmail.com>
2019-05-30 15:56:07 -0500

    css styling

--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -8,13 +8,13 @@ class Reviews extends React.Component {
         super(props);
 
         this.state = {
-            uuid: null,
+            uuid: 50,
             reviews: [{stars: 0, user: '', date: '', text: '', avatar: ''}],
         }
     }
 
     componentDidMount() {
-        axios.get('/reviews', {params: { uuid: 55 }})
+        axios.get('/reviews', {params: { uuid: this.state.uuid }})
         .then((response) => {
             console.log(response.data.review);
             this.setState({
@@ -27,7 +27,6 @@ class Reviews extends React.Component {
     }
 
     avgStar() {
-        let output = '';
         let array = this.state.reviews.map((review) => {
             return Number(review.stars);
         });
@@ -81,20 +80,22 @@ class Reviews extends React.Component {
 
     render() {
         return(
-            <div>
+            <div className='container'>
                 <div>
-                <ReactStars count={5} size={24} color2={'#ffd700'} value={this.avgStar()} edit={false} />
+                <ReactStars count={5} size={24} color1={'#E8E8E8'} color2={'#101820'} value={this.avgStar()} edit={false} />
                 </div>
                 {this.state.reviews.map( (review, index) => {
                     return(
-                        <div>
+                        <div className='one-review'>
                             <img key={index} src={review.avatar} alt="Avatar" className="images"/>
-                            <div key={index} className='names'>{review.user}</div>
-                            <div key={index} className='dates'>{this.dateChanger(review.date)}</div>
-                            <div className='startext'>
-                            <ReactStars count={5} size={24} color2={'#ffd700'} value={review.stars} edit={false} />
-                                <span key={index} className='text'>{review.text}</span>
-                            </div>
+                            <div className='review-data'>
+                                <div key={index} className='names'>{review.user}</div>
+                                <div key={index} className='dates'>{this.dateChanger(review.date)}</div>
+                                <span className='star-text'>
+                                    <ReactStars className='inline-stars' count={5} size={24} color1={'#E8E8E8'} color2={'#101820'} value={review.stars} edit={false} />
+                                    <div key={index} className='text'>{review.text}</div>
+                                </span>
+                            </div>    
                         </div>
                     )
                 }
