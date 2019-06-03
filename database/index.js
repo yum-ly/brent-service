2978a589354446002e84323e563af22c3dd4fff0
Brent Rusnell <blrusnell@gmail.com>
2019-06-03 14:05:00 -0500

    fixing db

--- database/index.js
+++ database/index.js
@@ -27,14 +27,14 @@ let Review = mongoose.model('Review', reviewSchema);
 // }
 
 //Pull one review from db
-// let findOne = (object, callback) => {
-//   Review.findOne(object, function(err, data) {
-//     if (err) {
-//       console.log(err);
-//     } 
-//     callback(null, data);
-//   });
-// }
+let findOne = (object, callback) => {
+  Review.findOne(object, function(err, data) {
+    if (err) {
+      console.log(err);
+    } 
+    callback(null, data);
+  });
+}
 
 
 // const MongoClient = require('mongodb').MongoClient;
@@ -49,30 +49,30 @@ let Review = mongoose.model('Review', reviewSchema);
 
 
 
-// module.exports.findOne = findOne;
+module.exports.findOne = findOne;
 // module.exports.findAll = findAll;
 
 
 
 ////////********* */DATABASE SEEDING! DONT TOUCH UNLESS NEED TO RESEED*******//////
-const seed = () => { fs.readFile('./data.json', (err, data) => {
-  //console.log(JSON.parse(data[0]), 'yo');
-  let parsedata = JSON.parse(data);
-  parsedata.map(e => {
+// const seed = () => { fs.readFile('./data.json', (err, data) => {
+//   //console.log(JSON.parse(data[0]), 'yo');
+//   let parsedata = JSON.parse(data);
+//   parsedata.map(e => {
    
-  const newRestaurant = new Review(e)
-  newRestaurant.save()
-  .then((response) => {
-    console.log(response);
-  })
-  .catch((error) => {
-    //console.log(error);
-  })
-})
-})
-}
+//   const newRestaurant = new Review(e)
+//   newRestaurant.save()
+//   .then((response) => {
+//     console.log(response);
+//   })
+//   .catch((error) => {
+//     //console.log(error);
+//   })
+// })
+// })
+// }
 
-seed()
+// seed()
 
 
 
